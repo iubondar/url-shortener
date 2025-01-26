f8975804fe85cfda3ec8a0e17e46fa86e79f12d4
Ivan Bondar <iubondar@gmail.com>
2025-01-26 13:39:25 +0300

    Add retrieve URL handler, fix namings

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/iubondar/url-shortener/internal/app/strings"
 )
 
-const shortURLLength int = 8
+const idLength int = 8
 
-var urls map[string]string
+var urlsToIds map[string]string
+var idsToURLs map[string]string
 
-func shortenHandler(res http.ResponseWriter, req *http.Request) {
+func createIdHandler(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
 		return
@@ -24,21 +24,47 @@ func shortenHandler(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	short, ok := urls[string(body)]
+	url := string(body)
+	id, ok := urlsToIds[url]
 	if !ok {
-		short = strings.RandString(shortURLLength)
-		urls[string(body)] = short
+		id = strings.RandString(idLength)
+		urlsToIds[url] = id
+		idsToURLs[id] = url
 		res.WriteHeader(http.StatusCreated)
 	}
 
-	res.Write([]byte(fmt.Sprintf("http://%s/%s", req.Host, short)))
+	res.Write([]byte("http://" + req.Host + "/" + id))
+}
+
+func retrieveURLHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := req.PathValue("id")
+	if len(id) == 0 {
+		http.Error(res, "Can't find id parameter in query path", http.StatusBadRequest)
+		return
+	}
+
+	url, ok := idsToURLs[id]
+	if !ok {
+		http.Error(res, "Can't find URL by id: "+id, http.StatusBadRequest)
+		return
+	}
+
+	res.Header().Add("Location", url)
+	res.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func main() {
-	urls = make(map[string]string)
+	urlsToIds = make(map[string]string)
+	idsToURLs = make(map[string]string)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, shortenHandler)
+	mux.HandleFunc(`/`, createIdHandler)
+	mux.HandleFunc(`/{id}`, retrieveURLHandler)
 
 	err := http.ListenAndServe(`localhost:8080`, mux)
 	if err != nil {
