2693459f594b7e869fa2be87c198327db324e3de
Ivan Bondar <iubondar@gmail.com>
2025-02-02 13:52:08 +0300

    Fix go vet check

--- internal/app/config/config_test.go
+++ internal/app/config/config_test.go
@@ -6,42 +6,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type envVars struct {
-	SERVER_ADDRESS string
-	BASE_URL       string
-}
-
 func TestConfig_Load(t *testing.T) {
 
 	tests := []struct {
 		name    string
 		args    []string
-		envVars envVars
+		envVars Config
 		want    Config
 	}{
 		{
 			name:    "Defaults",
 			args:    nil,
-			envVars: envVars{SERVER_ADDRESS: "", BASE_URL: ""},
+			envVars: Config{ServerAddress: "", BaseURLAddress: ""},
 			want:    Config{ServerAddress: "localhost:8080", BaseURLAddress: "localhost:8080"},
 		},
 		{
 			name:    "Override with flags",
 			args:    []string{"-a", "localhost:8888", "-b", "localhost:8000"},
-			envVars: envVars{SERVER_ADDRESS: "", BASE_URL: ""},
+			envVars: Config{ServerAddress: "", BaseURLAddress: ""},
 			want:    Config{ServerAddress: "localhost:8888", BaseURLAddress: "localhost:8000"},
 		},
 		{
 			name:    "Override with envs",
 			args:    []string{"-a", "localhost:8888", "-b", "localhost:8000"},
-			envVars: envVars{SERVER_ADDRESS: "localhost:8800", BASE_URL: "localhost:8808"},
+			envVars: Config{ServerAddress: "localhost:8800", BaseURLAddress: "localhost:8808"},
 			want:    Config{ServerAddress: "localhost:8800", BaseURLAddress: "localhost:8808"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Setenv("SERVER_ADDRESS", tt.envVars.SERVER_ADDRESS)
-			t.Setenv("BASE_URL", tt.envVars.BASE_URL)
+			t.Setenv("SERVER_ADDRESS", tt.envVars.ServerAddress)
+			t.Setenv("BASE_URL", tt.envVars.BaseURLAddress)
 
 			var c Config
 			c.Load("Test", tt.args)
