14c391dd4cc0906bf5ef2dce9a4990bb31340776
Ivan Bondar <iubondar@gmail.com>
2025-02-16 17:50:18 +0300

    Add shorten handler with json support

--- internal/app/router/default.go
+++ internal/app/router/default.go
@@ -11,13 +11,15 @@ func Default(baseURL string) chi.Router {
 
 	repo := storage.NewSimpleRepository()
 
-	CreateIDHandler := handlers.NewCreateIDHandler(repo, baseURL)
+	createIDHandler := handlers.NewCreateIDHandler(repo, baseURL)
+	shortenHandler := handlers.NewShortenHandler(repo, baseURL)
 	retrieveURLHandler := handlers.NewRetrieveURLHandler(repo)
 
 	r := chi.NewRouter()
 
 	r.Use(logging.WithLogging)
-	r.Post("/", CreateIDHandler.CreateID)
+	r.Post("/", createIDHandler.CreateID)
+	r.Post("/api/shorten", shortenHandler.Shorten)
 	r.Get("/{id}", retrieveURLHandler.RetrieveURL)
 
 	return r
