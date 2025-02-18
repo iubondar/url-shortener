f38e183d6b886afb78e146d82fe05eada1cd1d74
Ivan Bondar <iubondar@gmail.com>
2025-02-18 14:43:10 +0300

    Add gzip support

--- internal/app/router/default.go
+++ internal/app/router/default.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/iubondar/url-shortener/internal/api/handlers"
 	"github.com/iubondar/url-shortener/internal/app/storage"
+	"github.com/iubondar/url-shortener/internal/compress"
 	"github.com/iubondar/url-shortener/internal/logging"
 )
 
@@ -17,7 +18,7 @@ func Default(baseURL string) chi.Router {
 
 	r := chi.NewRouter()
 
-	r.Use(logging.WithLogging)
+	r.Use(logging.WithLogging, compress.WithGzipCompression)
 	r.Post("/", createIDHandler.CreateID)
 	r.Post("/api/shorten", shortenHandler.Shorten)
 	r.Get("/{id}", retrieveURLHandler.RetrieveURL)
