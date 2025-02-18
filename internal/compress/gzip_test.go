d69fd8f69f1eb255ed64cb97cc8f1e212230e26e
Ivan Bondar <iubondar@gmail.com>
2025-02-18 22:36:33 +0300

    Add gzip tests

---
+++ internal/compress/gzip_test.go
@@ -0,0 +1,78 @@
+package compress
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGzipCompression(t *testing.T) {
+	requestBody := `
+		<html><body><h1>Hello world!</h1></body></html>
+	`
+
+	successBody := `{
+		"result": "https://127.0.0.1/abcdef11"
+	}`
+
+	echo := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set(contentType, "application/json")
+		io.WriteString(w, successBody)
+	})
+	handler := WithGzipCompression(echo)
+
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
+
+	t.Run("sends_gzip", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+		zb := gzip.NewWriter(buf)
+		_, err := zb.Write([]byte(requestBody))
+		require.NoError(t, err)
+		err = zb.Close()
+		require.NoError(t, err)
+
+		r := httptest.NewRequest("POST", srv.URL, buf)
+		r.RequestURI = ""
+		r.Header.Set(contentEncoding, "gzip")
+		r.Header.Set(acceptEncoding, "")
+
+		resp, err := http.DefaultClient.Do(r)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+
+		defer resp.Body.Close()
+
+		b, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		assert.JSONEq(t, successBody, string(b))
+	})
+
+	t.Run("accepts_gzip", func(t *testing.T) {
+		buf := bytes.NewBufferString(requestBody)
+		r := httptest.NewRequest("POST", srv.URL, buf)
+		r.RequestURI = ""
+		r.Header.Set(contentType, "text/html")
+		r.Header.Set(acceptEncoding, "gzip")
+
+		resp, err := http.DefaultClient.Do(r)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+
+		defer resp.Body.Close()
+
+		zr, err := gzip.NewReader(resp.Body)
+		require.NoError(t, err)
+
+		b, err := io.ReadAll(zr)
+		require.NoError(t, err)
+
+		assert.JSONEq(t, successBody, string(b))
+	})
+}
