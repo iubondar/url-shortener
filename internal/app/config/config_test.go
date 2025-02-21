7e149c1afc981e2aff06ab435b59b53ef0055da2
Ivan Bondar <iubondar@gmail.com>
2025-02-21 14:35:12 +0300

    Add file storage path to configuration

--- internal/app/config/config_test.go
+++ internal/app/config/config_test.go
@@ -17,26 +17,27 @@ func TestConfig_Load(t *testing.T) {
 		{
 			name:    "Defaults",
 			args:    nil,
-			envVars: Config{ServerAddress: "", BaseURLAddress: ""},
-			want:    Config{ServerAddress: "localhost:8080", BaseURLAddress: "localhost:8080"},
+			envVars: Config{ServerAddress: "", BaseURLAddress: "", FileStoragePath: ""},
+			want:    Config{ServerAddress: "localhost:8080", BaseURLAddress: "localhost:8080", FileStoragePath: "./storage/storage.txt"},
 		},
 		{
 			name:    "Override with flags",
-			args:    []string{"-a", "localhost:8888", "-b", "localhost:8000"},
-			envVars: Config{ServerAddress: "", BaseURLAddress: ""},
-			want:    Config{ServerAddress: "localhost:8888", BaseURLAddress: "localhost:8000"},
+			args:    []string{"-a", "localhost:8888", "-b", "localhost:8000", "-f", "st/base.txt"},
+			envVars: Config{ServerAddress: "", BaseURLAddress: "", FileStoragePath: ""},
+			want:    Config{ServerAddress: "localhost:8888", BaseURLAddress: "localhost:8000", FileStoragePath: "st/base.txt"},
 		},
 		{
 			name:    "Override with envs",
-			args:    []string{"-a", "localhost:8888", "-b", "localhost:8000"},
-			envVars: Config{ServerAddress: "localhost:8800", BaseURLAddress: "localhost:8808"},
-			want:    Config{ServerAddress: "localhost:8800", BaseURLAddress: "localhost:8808"},
+			args:    []string{"-a", "localhost:8888", "-b", "localhost:8000", "-f", "st/base.txt"},
+			envVars: Config{ServerAddress: "localhost:8800", BaseURLAddress: "localhost:8808", FileStoragePath: "./ddd/ttt.txt"},
+			want:    Config{ServerAddress: "localhost:8800", BaseURLAddress: "localhost:8808", FileStoragePath: "./ddd/ttt.txt"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Setenv("SERVER_ADDRESS", tt.envVars.ServerAddress)
 			t.Setenv("BASE_URL", tt.envVars.BaseURLAddress)
+			t.Setenv("FILE_STORAGE_PATH", tt.envVars.FileStoragePath)
 
 			var c Config
 			c.Load("Test", tt.args)
